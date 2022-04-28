baecf3f99c7903e0c73144abed20765dc4a3afc5
Pheng <phengyangster9@gmail.com>
2022-04-28 14:39:08 -0700

    adding read file code

---
+++ 5_1.py
@@ -0,0 +1,30 @@
+
+file_name = "./infile.txt"
+infile = open(file_name, 'r')
+names = [line.rstrip() for line in infile]
+print("names: ", names)
+
+infile.close()
+
+def main():
+    display_with_forloop(file_name)
+    print()
+
+def display_with_forloop(file_name):
+    infile = open_file(file_name, 'r')
+    for line in infile:
+        print(line, end="") 
+    infile.close() 
+
+def display_with_list_comprehension(file_name):
+    infile = open_file(file_name, 'r')
+    items = [line.rstrip() for line in infile]
+    print("items: ", items)
+    infile.close()
+
+def open_file(file_name, mode):
+    return open(file_name, mode)
+
+
+main() 
+
